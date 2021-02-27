65d86bb433a31e19a91b9fe563233b1bd42e463d
Seth Troisi <sethtroisi@google.com>
2021-02-27 02:18:28 -0800

    misc: module_search_stack

--- misc/modulo_check.py
+++ misc/modulo_check.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Seth Troisi
+# Copyright 2021 Seth Troisi
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -31,8 +31,6 @@ def modulo_search(p, a, l, r):
         if a <= r - test:
             return mult + 1
 
-    print("\t\tdelta:", r - l)
-
     new_a = a - (p % a)
     assert 0 <= new_a < a, (a, new_a)
     k = modulo_search(a, new_a, l % a, r % a)
@@ -41,14 +39,60 @@ def modulo_search(p, a, l, r):
     mult = (tl - 1) // a + 1
     return mult
 
+def unwind(k, stack):
+    for p, a, l in reversed(stack):
+        k = (k * p + l - 1) // a + 1
+    return k
+
+def modulo_search_stack(p, a, l, r, stack):
+    if l == 0:
+        return unwind(0, stack)
+
+    if 2*a > p:
+        a, l, r = p - a, p - r, p - l
+
+    if a <= r:
+        # mult = (l - 1) // a + 1;
+        # test = mult * a
+        # if test <= r:
+        #   return mult
+
+        mult = (l - 1) // a
+        test = mult * a
+        assert mult < l
+        if a <= r - test:
+            return unwind(mult + 1, stack)
+
+    new_a = a - (p % a)
+    assert 0 <= new_a < a, (a, new_a)
+    # k = modulo_search(a, new_a, l % a, r % a, stack)
+    # tl = k * p + l
+    # return (tl - 1) // a + 1
+
+    stack.append((p, a, l))
+    return modulo_search_stack(a, new_a, l % a, r % a, stack)
+
+
 
 import sympy
-primes = list(sympy.primerange(2, 10 ** 5))
-
 s = 0
-for p in primes[-100:]:
+for p in list(sympy.primerange(10 ** 4, 10 ** 5))[-100:]:
     s += modulo_search(p, 123, 1000, 1010)
-    print("\tprime:", p, "search:", s)
-
 print()
 print(s, "vs 780012")
+
+
+import time
+primes = list(sympy.primerange(10 ** 4, 2 * 10 ** 6))
+s = 0
+t0 = time.time()
+for p in primes:
+    s += modulo_search(p, 123, 1000, 1010)
+
+t1 = time.time()
+for p in primes:
+    s -= modulo_search_stack(p, 123, 1000, 1010, [])
+
+t2 = time.time()
+
+print (f"diff: {s} | {t1 - t0:.3f} vs {t2 - t1:.3f}")
